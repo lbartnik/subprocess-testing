2e14b964c43e4ea4a5512360de4cfa2b5bea543a
Lukasz A. Bartnik <l.bartnik@gmail.com>
2017-01-08 09:38:52 -0800

    add an example

--- child/child.cpp
+++ child/child.cpp
@@ -1,6 +1,19 @@
 // child.cpp : Defines the entry point for the console application.
 //
 
+/*
+ h <- spawn_process("c:/Users/lbart/Source/subprocess-testing/x64/Debug/child.exe")
+
+ #h <- spawn_process(file.path(R.home("bin"), "R.exe"), "--slave")
+ process_send_signal(h, CTRL_C_EVENT)
+ process_send_signal(h, CTRL_BREAK_EVENT)
+ h
+
+ process_write(h, "x\n")
+ process_read(h)
+*/
+
+
 #include "stdafx.h"
 
 #include <csignal>
@@ -29,6 +42,8 @@ int main()
   signal(SIGBREAK, signal_handler);
 #endif
   /* etc */
+
+  cout << "starting" << endl;
 
   while (!quit)
   {
